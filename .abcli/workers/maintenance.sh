78e1897b5e094ed464030c3c7545b860d49186ad
kamangir <arash@kamangir.net>
2022-09-09 22:04:02 -0700

    validating blue-worker - kamangir/bolt#700

--- .abcli/workers/maintenance.sh
+++ .abcli/workers/maintenance.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
 function blue_worker_maintenance() {
-    abcli_log "blue_worker: maintenance: None."
+    abcli_log "blue_worker: maintenance: ⚙️"
 }
 
 blue_worker_maintenance
